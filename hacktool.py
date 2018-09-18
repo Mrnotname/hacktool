73fd70651a86f1cf2a8e08d595ea9098ce2203f1
MR.ID <34278330+Mrnotname@users.noreply.github.com>
2018-09-19 03:20:53 +0700

    Add files via upload

---
+++ hacktool.py
@@ -0,0 +1,21 @@
+print ("--------------------------------------------")
+print  ("++++++++++++||prank tools Hacker||++++++++++")
+print  ("-------------------------------------------")
+print  ('===========================================')
+print ("=======Pergunakan tools ini semestinya=======")
+print ("======jangan ganti cop coding MASTER=========")
+print ("=====kalau mau delover serta nama pembuat====")
+print ("=========coding by Mr ID/'INDONESIA/'========")
+print ("=============================================")
+print ("============$ Pasukan Termux Tool============")
+print ("=============================================")
+
+# mengambil input
+
+target = input ("target: ")
+jenis_serangan = input ("serang: ")
+
+# Menampilakan Output
+print ("target kamu ",target,"sedang diserang",jenis_serangan,"target PROSES SCANING.......",target,"DONE",target,"keamanannya tidak dapat di masuki maaf *-*")
+
+#coding prank programer 
